4b2dbe3794b46d6a06f4411aa1646698bec3391e
Nick Kletnoi <nick.kletnoi@gmail.com>
2022-03-05 16:21:56 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,5 +2,5 @@ variable "region" { default = "us-east-1" }
 variable "environment" { default = "" }
 variable "project" { default = "javaweb" }
 variable "platform" { default = "Devops" }
-variable "vpc_id" { default = "vpc-072ecaa60abe662d4" }
+variable "vpc_id" { default = "vpc-0d9f4187edd1bbd6b" }
 variable "team" { default = "devops4solutions" }
