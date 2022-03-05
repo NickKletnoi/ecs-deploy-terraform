f1499e7d75c31016e98d950be5ff33c128ec263e
Nick Kletnoi <nick.kletnoi@gmail.com>
2022-03-05 16:43:29 -0500

    Update data.tf

--- modules/ecs-fargate-service/data.tf
+++ modules/ecs-fargate-service/data.tf
@@ -1,11 +1,12 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_vpc" {
+  name = "aws_vpc"
   id = "vpc-0d9f4187edd1bbd6b"
 }
 
 data "aws_subnet_ids" "public" {
-  vpc_id = data.aws_vpc.id
+ vpc_id = data.aws_vpc.id
  tags = {
     Name = "*Public*"
   }
