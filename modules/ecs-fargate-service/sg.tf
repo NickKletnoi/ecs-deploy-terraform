71638e17db6b10fdc36a05a0489c659914dfaec7
Nick Kletnoi <nick.kletnoi@gmail.com>
2022-03-05 16:47:28 -0500

    Update sg.tf

--- modules/ecs-fargate-service/sg.tf
+++ modules/ecs-fargate-service/sg.tf
@@ -32,5 +32,5 @@ resource "aws_security_group" "allow-external" {
   }
 
   name   = "${var.project}-${var.environment}_allow-external-ecs"
-  vpc_id = data.aws_vpc.selected.id
+  vpc_id = "vpc-0d9f4187edd1bbd6b"
 }
